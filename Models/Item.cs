266910f70589e3def3bde1afc9150ff538855fa1
JonesKwameOsei <oseikwamejones@gmail.com>
2025-10-27 05:15:07 +0000

    Add project files.

---
+++ Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoppingCartApp.Models;
+
+public class Item
+{
+    public int Id { get; set; }
+    public string? name { get; set; }
+    public int Quantity { get; set; }
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal price { get; set; }
+
+    // Foreign key to Product
+    public int ProductId { get; set; }
+    public Product? Product { get; set; }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoppingCartApp.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string? ProductName { get; set; }
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Price { get; set; }
+    public string? ProductImage { get; set; }
+    public ICollection<Item>? Items { get; set; } = new List<Item>();
+}
