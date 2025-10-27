7982935156f4118a23c16ca29d70806e269b077d
JonesKwameOsei <oseikwamejones@gmail.com>
2025-10-27 05:42:23 +0000

    Add controllers and DbContext for Item and Product CRUD
    Added `ItemsController` to handle CRUD operations for `Item`
    entities, including listing, details, creation, editing, and
    deletion. Integrated dependency injection for `ShoppingCartAppContext`.
    
    Added `ProductsController` to manage CRUD operations for
    `Product` entities with similar functionality and validation
    as `ItemsController`.
    
    Introduced `ShoppingCartAppContext` as the Entity Framework
    Core database context, defining `DbSet` properties for
    `Product` and `Item` entities. Configured dependency injection
    and initialized with `DbContextOptions`.

---
+++ Data/ShoppingCartAppContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCartApp.Models;
+
+namespace ShoppingCartApp.Data
+{
+    public class ShoppingCartAppContext : DbContext
+    {
+        public ShoppingCartAppContext (DbContextOptions<ShoppingCartAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ShoppingCartApp.Models.Product> Product { get; set; } = default!;
+        public DbSet<ShoppingCartApp.Models.Item> Item { get; set; } = default!;
+    }
+}
