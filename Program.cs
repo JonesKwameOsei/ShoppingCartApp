97f564f3184a3a2c28cae4764807c97bb9478dc2
JonesKwameOsei <oseikwamejones@gmail.com>
2025-10-29 05:22:37 +0000

    Refactor Program.cs for improved structure and clarity
    Encapsulated application setup within a `Program` class and
    `Main` method. Updated `using` directives for clarity. Moved
    service registrations and HTTP pipeline configuration into
    `Main`. Added `CartService` to DI container. Improved
    middleware setup and route mapping. Enhanced readability
    and organization of the application entry point.

--- Program.cs
+++ Program.cs
@@ -1,33 +1,47 @@
 using Microsoft.EntityFrameworkCore;
+using ShoppingCartApp.Controllers;
 using ShoppingCartApp.Data;
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<ShoppingCartAppContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("ShoppingCartAppContext") ?? throw new InvalidOperationException("Connection string 'ShoppingCartAppContext' not found.")));
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddDbContext<ShoppingCartAppContext>(options =>
+            options.UseSqlite(builder.Configuration.GetConnectionString("ShoppingCartAppContext") ?? throw new InvalidOperationException("Connection string 'ShoppingCartAppContext' not found.")));
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddScoped<CartService>();
+
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapStaticAssets();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}")
+            .WithStaticAssets();
+
+
+        app.Run();
+    }
+
 }
-
-app.UseHttpsRedirection();
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-
-app.Run();
