1a63e3b2789bf813a43ecb7cced75cda003a95ed
JonesKwameOsei <oseikwamejones@gmail.com>
2025-11-01 02:48:28 +0000

    Refactor HomeController to support database integration
    Updated `HomeController` to include `ShoppingCartAppContext` for database interaction. Modified the `Index` action to asynchronously retrieve and display products from the database. Added necessary `using` directives for Entity Framework Core. Made minor formatting updates to `Privacy` and `Error` methods. Preserved logging functionality with `ILogger<HomeController>`.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,37 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCartApp.Data;
 using ShoppingCartApp.Models;
+using System.Diagnostics;
 
-namespace ShoppingCartApp.Controllers
+namespace ShoppingCartApp.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+    private readonly ShoppingCartAppContext _context;
+
+    public HomeController(ILogger<HomeController> logger, ShoppingCartAppContext context)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+        _context = context;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public async Task<IActionResult> Index()
+    {
+        // Get all products to display on home page
+        var products = await _context.Products.ToListAsync();
+        return View(products);
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
